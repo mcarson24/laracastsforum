b52eb3be05b126fe0a0c4c65867c66e526bd6d70
Matthew Carson <matthew24carson@gmail.com>
2017-05-30 17:38:42 -0400

    Add docblock to Channel.php

--- app/Channel.php
+++ app/Channel.php
@@ -6,11 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Channel extends Model
 {
+	/**
+	 * Set the route key name equal to the Channel's slug
+	 * 
+	 * @return string
+	 */
     public function getRouteKeyName()
     {
     	return 'slug';
     }
 
+    /**
+     * A channel has many threads.
+     * 
+     * @return Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function threads()
     {
     	return $this->hasMany(Thread::class);
