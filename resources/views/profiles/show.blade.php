01519532e0135ba548e98498abb1b80abbfd7141
Matthew Carson <matthew24carson@gmail.com>
2017-05-30 18:28:52 -0400

    Design updates

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -3,42 +3,45 @@
 @section('content')
 	
 	<div class="container">
-		<div class="page-header">
-			<h1>
-				{{ $profileUser->name }}
-				<small>joined us {{ $profileUser->created_at->diffForHumans() }}</small>
-			</h1>
-			
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<div class="page-header">
+					<h1>
+						{{ $profileUser->name }}
+						<small>joined us {{ $profileUser->created_at->diffForHumans() }}</small>
+					</h1>
+				</div>
+
+				@foreach ($threads as $thread)
+					<div class="panel panel-default">
+		                <div class="panel-heading">
+							<div class="level">
+								<span class="flex">
+									{{ $thread->title }} 	
+								</span>
+			                	<span class="creator-name">
+			                		{{ $thread->created_at->diffForHumans() }}
+		                		</span>
+		                	</div>
+		            	</div>
+
+		                <div class="panel-body">
+		                    {{ $thread->body }}
+		                    <hr>
+		                </div>
+
+						<div class="container">
+							<div class="row">
+			                    <div class="col-md-8">
+									{{ $threads->links() }}
+			                    </div>
+			                </div>
+		                </div>
+
+		            </div>
+				@endforeach
+			</div>
 		</div>
-
-		@foreach ($threads as $thread)
-			<div class="panel panel-default">
-                <div class="panel-heading">
-					<div class="level">
-						<span class="flex">
-							{{ $thread->title }} 	
-						</span>
-	                	<span class="creator-name">
-	                		{{ $thread->created_at->diffForHumans() }}
-                		</span>
-                	</div>
-            	</div>
-
-                <div class="panel-body">
-                    {{ $thread->body }}
-                    <hr>
-                </div>
-
-				<div class="container">
-					<div class="row">
-	                    <div class="col-md-8">
-							{{ $threads->links() }}
-	                    </div>
-	                </div>
-                </div>
-
-            </div>
-		@endforeach
-	</div>
+	</div>		
 
 @endsection
--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,31 +4,31 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Forum Threads</div>
-
-                    <div class="panel-body">
-                        @if ($threads->isEmpty())
-                            <p>There is nothing here. Why not start a thread of your own 
-                                <a href="{{ action('ThreadsController@create') }}">here</a>?
-                            </p>
-                        @endif
-                        @foreach ($threads as $thread)
+                @foreach ($threads as $thread)
+                    <div class="panel panel-default">   
+                        <div class="panel-heading">
+                            <div class="level">
+                                <h4 class="flex">
+                                    <a href="{{ action('ThreadsController@show', ['channel' => $thread->channel->slug, 'thread' => $thread]) }}">{{ $thread->title }}</a>
+                                </h4>
+                                <a href="{{ action('ThreadsController@show', [$thread->channel, $thread]) }}">
+                                    <strong>{{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}</strong>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="panel-body">
                             <article>
-                                <div class="level">
-                                    <h4 class="flex">
-                                        <a href="{{ action('ThreadsController@show', ['channel' => $thread->channel->slug, 'thread' => $thread]) }}">{{ $thread->title }}</a>
-                                    </h4>
-                                    <a href="{{ action('ThreadsController@show', [$thread->channel, $thread]) }}">
-                                        <strong>{{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}</strong>
-                                    </a>
-                                </div>
+                                
                                 <div class="body">{{ $thread->body }}</div>
-                                <hr>
                             </article>
-                        @endforeach
+                        </div>
                     </div>
-                </div>
+                    @endforeach
+                    @if ($threads->isEmpty())
+                        <p>There is nothing here. Why not start a thread of your own 
+                            <a href="{{ action('ThreadsController@create') }}">here</a>?
+                        </p>
+                    @endif
             </div>
         </div>
     </div>
