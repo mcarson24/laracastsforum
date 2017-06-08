24f9f286e47885bfa7c675668de754f869fe7c9d
Matthew Carson <matthew24carson@gmail.com>
2017-06-08 16:58:43 -0400

    Add method to link to model that was favorited.

--- app/Favorite.php
+++ app/Favorite.php
@@ -2,9 +2,23 @@
 
 namespace App;
 
+use App\User;
+use App\RecordsActivity;
 use Illuminate\Database\Eloquent\Model;
 
 class Favorite extends Model
 {
+	use RecordsActivity;
+
 	protected $fillable = ['user_id', 'favorited_id', 'favorited_type'];
+
+	/**
+	 * Get the model that was favorited.
+	 * 
+	 * @return Illuminate\Database\Eloquent\Relations\MorphTo
+	 */
+	public function favorited()
+	{
+		return $this->morphTo();
+	}
 }
