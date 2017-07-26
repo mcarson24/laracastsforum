26fa24d81f8ab33590b9871302dd973ef66821ac
Matthew Carson <matthew24carson@gmail.com>
2017-07-26 14:08:13 -0400

    Clean up MentionedUsersTest

--- tests/Feature/MentionedUsersTest.php
+++ tests/Feature/MentionedUsersTest.php
@@ -15,18 +15,15 @@ class MentionedUsersTest extends TestCase
     /** @test */
     public function mentioned_users_in_a_reply_are_notified()
     {
-        // Given I have a user named JohnDoe,
         $john = create(User::class, ['name' => 'JohnDoe']);
         $this->signIn($john);
-        // and another user JaneDoe
         $jane = create(User::class, ['name' => 'JaneDoe']);
-        // If we have a thread
+        
         $thread = create(Thread::class);
-        // And JohnDoe replies and mentions JaneDoe
         $reply = create(Reply::class, ['body' => 'Mentioning @JaneDoe @BobbyDoe @DorisDoe! right here.']);
         
         $this->postJson($thread->path(). '/replies', $reply->toArray()); 
-        // Then, Jane should be notified
+        
         $this->assertCount(1, $jane->notifications);
     }
 }
