ce02a8f1cc2f3a8af9eeb86141b0852806321595
Matthew Carson <matthew24carson@gmail.com>
2017-07-26 17:12:31 -0400

    Remove some nonsense

--- app/Reply.php
+++ app/Reply.php
@@ -51,7 +51,7 @@ class Reply extends Model
 
     public function mentionedUsers()
     {
-        preg_match_all('/\@([\w]+[\w]+)/', $this->body, $matches);
+        preg_match_all('/\@([\w]+)/', $this->body, $matches);
         
         return $matches[1];
     }
