ccc3e067f918505f0accd4525bc4c0039d8ac855
Matthew Carson <matthew24carson@gmail.com>
2017-08-24 15:58:00 -0400

    Don't show most popular threads if there are no scores

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -7,20 +7,22 @@
                 @include('threads._list')
                 {{ $threads->render() }}
             </div>
-	        <div class="col-md-4">
-	        	<div class="panel panel-default">
-	        		<div class="panel-heading">
-	        			Trending Threads
-	        		</div>
-	        		<div class="panel-body">
-	        			<div class="list-group">
-		        			@foreach ($trending as $thread)
-								<li class="list-group-item"><a href="{{ $thread->path }}">{{ $thread->title }}</a></li>
-		        			@endforeach
-	        			</div>
-	        		</div>
-	        	</div>
-	        </div>
+            @if (count($trending))
+		        <div class="col-md-4">
+		        	<div class="panel panel-default">
+		        		<div class="panel-heading">
+		        			Trending Threads
+		        		</div>
+		        		<div class="panel-body">
+		        			<div class="list-group">
+			        			@foreach ($trending as $thread)
+									<li class="list-group-item"><a href="{{ $thread->path }}">{{ $thread->title }}</a></li>
+			        			@endforeach
+		        			</div>
+		        		</div>
+		        	</div>
+		        </div>
+	        @endif
         </div>
     </div>
 @endsection
